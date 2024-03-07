07f43bb311f95fce6ee569af35aafcd0551da0a7
Gabriel Antunes <gabriel.antunes@archoffice.tech>
2024-03-07 17:22:55 -0300

    fix some errors trying deployment

--- index.js
+++
@@ -1,8 +0,0 @@
-(async () => {
-    const {
-        sequelize,
-    } = require("./src/layers/external/database/config/config.js");
-    const User = require("./src/layers/external/database/models/user-model");
-    const Product = require("./src/layers/external/database/models/product-model");
-    await sequelize.sync();
-})();

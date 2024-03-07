26ec666b9a945b3152f2c76d035329695261904b
Gabriel Antunes <gabriel.antunes@archoffice.tech>
2024-03-07 19:18:58 -0300

    fix

--- src/layers/external/database/config/config.js
+++ src/layers/external/database/config/config.js
@@ -3,6 +3,13 @@ const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(DATABASE_URL, {
     dialect: "postgres",
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    },
+    dialectModule: require("pg"),
 });
 
 module.exports = { sequelize };
